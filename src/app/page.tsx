ecac5bed944ffe55eebc030b0316a5ef2c4be35c
Aira Aria <127509175+JihadZaidan@users.noreply.github.com>
2025-05-14 12:24:51 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,1 @@
-import Button from "@/components/common/Button";
-import { Card } from "@/components/common/Card";
-import Navbar from "@/components/form/Navbar";
-import PrimaryHeader from "@/components/form/PrimaryHeader";
-import Items from "@/components/ui/Item";
 
-export default function Home() {
-  return (
-    <div className="w-full flex-row gap-4 p-10">
-      <Button/>
-    </div>
-  );
-}
